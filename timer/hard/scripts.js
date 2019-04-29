e8ded98af5fcd56468e94b6cdfb350ea4d7c7fcf
optimist23 <opt1m1st23@yandex.ru>
2019-04-30 00:14:20 +0500

    animation

--- timer/hard/scripts.js
+++ timer/hard/scripts.js
@@ -5,38 +5,16 @@ let btn = document.querySelector('.btn'),
 
  function driving() {
 	 let pos = 0;
-	requestAnimationFrame(driving);
-	//let id = setInterval(frame, 1);
-		if(pos == 1000) {
-			cancelAnimationFrame(requestAnimationFrame(driving));
-		} else {
-			pos++;
+	 requestAnimationFrame(function frame() {
+		 if(pos == 1000) {
+				cancelAnimationFrame(frame);
+		 } else {
+			pos += 4;
 			car.style.left = pos + 'px';
-		}
-}
-	// setInterval(function() {
-	// 	requestAnimationFrame(driving);
-	// 	let pos = 0;
-	// 		if(pos == 1000) {
-	// 			clearInterval(driving);
-	// 		} else {
-	// 			pos++;
-	// 			car.style.left = pos + 'px';
-	// 		}
-	// }, 100);
+			requestAnimationFrame(frame);
+		 }
 
-	// function driving() {
-	// 	requestAnimationFrame(driving);
-	// 	let pos = 0;
-	// 		id = setInterval(frame, 15);
-	// 	function frame(){
-	// 		if(pos == 1000) {
-	// 			clearInterval();
-	// 		} else {
-	// 			pos++;
-	// 			car.style.left = pos + 'px';
-	// 		}
-	// 	}
-	// }
+	 });
+}
 
 	btn.addEventListener('click', driving);
