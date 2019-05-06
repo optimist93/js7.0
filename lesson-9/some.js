1d09c057808b8b2065563f753677d8900b7715c7
optimist23 <opt1m1st23@yandex.ru>
2019-05-06 23:29:49 +0500

    add lesson-9

---
+++ lesson-9/some.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function() {
+'use strict';
+
+let age = document.getElementById('age');
+
+	function showUser(surname, name) {
+					alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+	}
+	showUser.apply(age, ['Бокачев', 'Роман']);
+
+
+
+}); // -> end scripts
